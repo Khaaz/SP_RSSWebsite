ba32bf68d27b4969ee6b9e03a994e7285c65d3f7
khaazz <khaaz.dev@gmail.com>
2018-12-04 23:29:44 +0100

    refactor in correct location

---
+++ Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaaz
+ * Date: 12/4/18
+ * Time: 11:27 PM
+ */
+
+class UserController
+{
+
+}
---
+++ DAL/Class/News.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaaz
+ * Date: 12/4/18
+ * Time: 11:26 PM
+ */
+
+class News
+{
+
+}
